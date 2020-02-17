13c33972d48a7a8ac2f40ce875e190db34f13d4e
veronika333 <vera3@inbox.ru>
2020-02-17 20:08:32 +0200

    Scroll up button added

---
+++ veronika/script.js
@@ -0,0 +1,40 @@
+/*$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+}); 
+
+jQuery(document).ready(function() {
+    
+    var btn = $('#button');
+  
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+  
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+  
+  }); */
+
+
+  //Get the button:
