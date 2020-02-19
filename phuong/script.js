411468236fed5a2a1be4fa5e140c9902bea2073a
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-02-19 19:51:58 +0200

    made overlay menu

--- phuong/script.js
+++ phuong/script.js
@@ -4,3 +4,20 @@ $(function() {
     return false;
   });
 });
+
+// function myFunction() {
+//   var x = document.getElementById("mobile-nav");
+//   if (x.className === "mobile-menu") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "mobile-menu";
+//   }
+// }
+function openNav() {
+  document.getElementById("myNav").style.width = "60%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
