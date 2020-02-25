e4e0c95d51960bae20714f49663a71f15245b90d
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-02-25 19:32:01 +0200

    finished making the website responsive
    wrote html, css and added javascript for contact.html

--- phuong/script.js
+++ phuong/script.js
@@ -1,3 +1,4 @@
+//Scroll down button
 $(function() {
   $(".scroll-down").click(function() {
     $("html, body").animate({ scrollTop: $("#intro").offset().top }, "slow");
@@ -5,19 +6,18 @@ $(function() {
   });
 });
 
-// function myFunction() {
-//   var x = document.getElementById("mobile-nav");
-//   if (x.className === "mobile-menu") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "mobile-menu";
-//   }
-// }
+//Overlay menu
 function openNav() {
   document.getElementById("myNav").style.width = "60%";
 }
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+//Display range value
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
