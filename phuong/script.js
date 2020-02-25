39a87081cbbaecead8d5a62bda63f2271ec00316
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-02-25 22:01:03 +0200

    making back to top button

--- phuong/script.js
+++ phuong/script.js
@@ -21,3 +21,19 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 };
+
+var myBtn = document.getElementById("back-to-top");
+window.onscroll = function() {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
