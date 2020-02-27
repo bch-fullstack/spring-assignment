684a70793c703c035f458ebc8be89d25c46b94e4
otsolap <59653348+otsolap@users.noreply.github.com>
2020-02-27 16:22:51 +0200

    Copywrite adjustments - Fontawesome included. + Konami code!
    Html:
    
    - added social media icons utilising awesome font's icons.
    
    - did some changes to copywriting. Less text and 1 more url link.
    
    CSS:
    
    - Tested around with font heights, spaces, and so forward.
    
    - The trainee-developer needs more indepth understanding when adding height is necessary and when it is necessary what is the fine line between vh, rem or pixel.
    
    Javascript:
    
    -added the konami code.

--- otso/js/scripts.js
+++ otso/js/scripts.js
@@ -1,35 +1,55 @@
+/* Scroll to top button function */
+
 jQuery(document).ready(function() {
-  
-    var btn = $('#topBtn');
-  
-    $(window).scroll(function () {
-      let height = $(window).scrollTop();
-      if (height > 150) {
-        $("#topBtn").fadeIn();
-      } else {
-        $("#topBtn").fadeOut();
-      }
-    
-    });
-  
-    btn.on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({scrollTop:0}, '300');
-    });
-  
+  var btn = $("#topBtn");
+
+  $(window).scroll(function() {
+    let height = $(window).scrollTop();
+    if (height > 150) {
+      $("#topBtn").fadeIn();
+    } else {
+      $("#topBtn").fadeOut();
+    }
   });
 
-  var doc = new jsPDF();
-  var specialElementHandlers = {
-      '#toPdf': function (element, renderer) {
-          return true;
-      }
-  };
-  
-  $('#pdfBtn').click(function () {
-      doc.fromHTML($('#toPdf').html(), 15, 15, {
-          'width': 170,
-              'elementHandlers': specialElementHandlers
-      });
-      doc.save('Otso-Lappalainen-CV-2020.pdf');
+  btn.on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
   });
+});
+
+/* Konami Code */
+
+var pattern = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+var current = 0;
+
+var keyHandler = function(event) {
+  // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+  if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+    current = 0;
+    return;
+  }
+
+  // Update how much of the pattern is complete
+  current++;
+
+  // If complete, alert and reset
+  if (pattern.length === current) {
+    current = 0;
+    window.alert("RIP Kazuhisa Hashimoto");
+  }
+};
+
+// Listen for keydown events
+document.addEventListener("keydown", keyHandler, false);
