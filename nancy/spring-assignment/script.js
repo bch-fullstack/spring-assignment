39f40e1d128a0a6f60b85ddf87536d8e96be5a5d
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-02-27 12:14:52 +0200

    jquery smooth scrow and animation

--- nancy/spring-assignment/script.js
+++ nancy/spring-assignment/script.js
@@ -1,7 +1,19 @@
 $(document).ready (function(){
- $('menu-toggler').on('click', function(){
+ $('.menu-toggler').on('click', function(){
  $(this).toggleClass('open');
  $('.topNav').toggleClass('open');
- })
+ });
+
+ $('.topNav .nav-links').on('click', function(){
+    $('.menu-toggler').removeClass('open');
+    $('.topNav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate(keyframes,{
+            scrollTop: $($(this).attr('href')).offset().top - 100
+
+        }, Options, 2000);
+        });
 
 });
