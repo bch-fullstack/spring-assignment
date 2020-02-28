16307f1edce251ab3ca36a0627c8437a735e0669
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-02-28 10:58:30 +0200

    about me page customization

--- aghogho/js/script.js
+++ aghogho/js/script.js
@@ -1,5 +1,10 @@
 //scroll to top
 const btnScrollToTop = document.getElementById("btnScrollToTop");
 btnScrollToTop.addEventListener("click", function() {
-  window.scrollTo(0, 0);
+  //window.scrollTo(0, 0);
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
 });
