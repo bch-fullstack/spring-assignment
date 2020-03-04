356ae24c5477840c144a0ecf461db2307f9d6b4d
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-03-03 14:12:44 -1100

    scroll to top

--- ebou/script.js
+++ ebou/script.js
@@ -24,5 +24,15 @@ $(".image-slider").slick({
     }
   ]
 });
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scroll-btn").style.display = "block";
+  } else {
+    document.getElementById("scroll-btn").style.display = "none";
+  }
+};
 
-
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox and Opera
+}
